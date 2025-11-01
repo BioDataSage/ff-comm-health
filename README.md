34247e551f8a1d3b2f5db0306f8b07bb7739c818
Derrick Nyarko <nyarderr@gmail.com>
2025-11-01 19:47:40 +0000

    commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# ff-comm-health
+# Vulnerability Drivers Analysis
+
+## Goal
+
+This project moves beyond just where vulnerability is and answers "why"
+
+## Concept
+
+Symptom vs. Diagnosis
+
+-   The overall SVI score will represent symptoms
+-   The four theme scores will represent the diagnosis
+
+This project will aim to provide a visualisation that shows both the symptom and diagnosis at a glance (preferably a map)
+
+## Question
+
+For a particular state, what is the main driver of Social vulnerability??
--- hello.R
+++ hello.R
@@ -1,1 +1,37 @@
-# Hi guys this is a test 
+##====================================
+# Drivers of Vulnerability Analysis
+#=====================================
+
+## so we want to look at vulnerability driver status
+library(tidyverse)
+
+
+symptoms <- Rdata |> 
+  select(STATE,RPL_THEMES, RPL_THEME1, RPL_THEME2, RPL_THEME3, RPL_THEME4) |> 
+  rename("SVI"=RPL_THEMES,
+         "Socioecomic"=RPL_THEME1,
+         "household"=RPL_THEME2,
+         "racial"=RPL_THEME3,
+         "housing"=RPL_THEME4) |> 
+  pivot_longer(-c(STATE),
+               names_to = "variables",
+               values_to = "index") |> 
+  group_by(STATE,variables) |> 
+  summarise(index=mean(index), .groups = "drop") |> 
+  pivot_wider(id_cols = STATE,
+              names_from = variables,
+              values_from = index)
+
+
+
+
+
+
+
+
+
+
+
+# 
+#   group_by(STATE) |> 
+#   summarise(SVI = mean(RPL_THEMES))
